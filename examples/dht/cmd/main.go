639573a38e5d027d616ff0895ed81f495bdaffc7
liangchuan <cc14514@icloud.com>
2018-08-31 23:06:25 +0800

    fix

--- examples/dht/cmd/main.go
+++ examples/dht/cmd/main.go
@@ -165,7 +165,7 @@ func start(ctx *cli.Context) {
 				return
 			}
 			if bytes.Equal(buff[len(buff)-1:], []byte{1}[:]) {
-				fmt.Println("------------------------------->")
+				fmt.Println("------------------------------->",buff[len(buff)-3:])
 				buff = buff[:len(buff)-1]
 			}
 			t, _ := f.Write(buff)
